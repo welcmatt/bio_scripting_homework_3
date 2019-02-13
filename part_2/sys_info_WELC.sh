21ce5596fd8ad89fbebd6fc8d59d1b04f3d0d8ec
Auburn class account - Auburn <aubcls72@dmcvlogin1.asc.edu>
2019-02-13 15:17:39 -0600

    Adding content

--- part_2/sys_info_WELC.sh
+++ part_2/sys_info_WELC.sh
@@ -1,1 +1,19 @@
-#/bin/bash
+#!/bin/bash
+
+#create variable with current time
+currenttime=`date | awk '{print $4,$5}'`
+
+#create variable with current date
+currentdate=`date | awk '{print $1,$2,$3}'`
+
+#create variable with system uptime
+currentuptime=uptime
+
+#create variable storing current online users
+currentusers=w
+
+printf "Current time: ${currenttime}/n
+Current date: ${currentdate}/n
+Current uptime: ${currentuptime}/n
+Current users:${currentusers}/n" >> sys_infor_WELC.output
+
