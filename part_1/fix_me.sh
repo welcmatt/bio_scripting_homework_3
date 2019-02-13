ce6a4fa905a401e8f912a21ab2f6f1651ff6a89a
Auburn class account - Auburn <aubcls72@dmcvlogin1.asc.edu>
2019-02-13 14:53:46 -0600

    Fixed echo issue
    switched echo out for read -

---
+++ part_1/fix_me.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Reading data from the user's input
+read -p 'Enter a : ' a
+read -p 'Enter b : ' b
+
+add=$((a + b))
+
+echo Addition of a and b are $add
+
+sub=$((a - b))
+echo Subtraction of a and b are $sub
+
+mul=$((a * b))
+echo Multiplication of a and b are $mul
+
+div=$((a / b))
+echo division of a and b are $div
+
+mod=$((a % b))
+echo Modulus of a and b are $mod
+
+((++a))
+echo Increment operator when applied on "a" results into a = $a
+
+((--b))
+echo Decrement operator when applied on "b" results into b = $b
+
+((--c))
+echo What was the default value \ of c if its value is now $c\?
